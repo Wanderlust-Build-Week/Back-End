75df2ed3eda4cdb27752eb9e150180b157b641e8
Chase Wallace <wallacecs007@gmail.com>
2020-02-06 21:01:50 -0700

    Turning console logging on

--- routes/tours-router.js
+++ routes/tours-router.js
@@ -21,6 +21,8 @@ router.post('/upload', authMiddleware, async (req, res, next) => {
 
     const tour = req.body.tour;
 
+    console.log(tour)
+
     if( !tour || !tour.title || !tour.photo || !tour.location || !tour.description || !tour.duration || !tour.guide) {
         return res.status(401).json({message: 'Please include all required information for a tour.'})
     }
